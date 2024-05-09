4c53d2d3bbfb9b265cd1a199ba7fe051562129d9
nazirovdev <nazirazhari5865@gmail.com>
2024-05-09 17:17:17 +0700

    initial commit

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,21 @@
+import Image from "next/image";
+
 const Homepage = () => {
-  return <div>Hello World</div>;
+  return (
+    <div className="flex flex-col md:flex-row py-4 md:py-0">
+      <div className="w-full h-52 xl:h-[500px] md:w-1/2 relative">
+        <Image src="/hero.png" alt="My Hero Photo" fill objectFit="contain" />
+      </div>
+      <div className="w-full min-h-48 md:w-1/2 flex flex-col justify-center gap-y-4 mt-10 md:mt-0">
+        <h1 className="text-2xl md:text:6xl font-bold">Crafting Digital Experiences, Designing Tomorrow.</h1>
+        <p>Welcome to my digital canvas, where innovation and creativity converage, with a keen eye for aesthetics and mastery of code, my portfolio showcases a diverse collection of projects that reflect my commitment to excellence.</p>
+        <div className="flex flex-col md:flex-row gap-2">
+          <button className="p-2 md:p-4 bg-black text-white rounded-md font-bold">View My Work</button>
+          <button className="p-2 md:p-4 bg-transparent text-black ring-1 ring-black rounded-md font-bold">Contact Me</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Homepage;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,162 @@
+"use client"
+
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+import React, { useEffect, useRef, useState } from 'react'
+import { motion } from 'framer-motion'
+
+const links = [
+  {
+    path: '/',
+    title: 'Home'
+  },
+  {
+    path: '/about',
+    title: 'About'
+  },
+  {
+    path: '/contact',
+    title: 'Contact'
+  },
+  {
+    path: '/portfolio',
+    title: 'Portfolio'
+  },
+]
+
+const variantNav = {
+  close: {
+    x: '100vw'
+  },
+  open: {
+    x: 0,
+    transition: {
+      when: 'beforeChildren',
+      staggerChildren: 0.3
+    }
+  }
+}
+
+const variantNavItem = {
+  open: {
+    x: 0,
+    opacity: 1
+  },
+  close: {
+    x: -20,
+    opacity: 0
+  }
+}
+
+const variantTopNav = {
+  open: {
+    y: 13.5
+  },
+  close: {
+    y: 0
+  }
+}
+
+const variantCenterNav = {
+  open: {
+    opacity: 1
+  },
+  close: {
+    opacity: 1
+  }
+}
+
+const variantBottomNav = {
+  open: {
+    y: -13.5
+  },
+  close: {
+    y: 0
+  }
+}
+
+export default function Navbar() {
+  const [open, setOpen] = useState(false)
+  const [scroll, setScroll] = useState(false)
+  const pathname = usePathname()
+  const ref = useRef()
+
+  const onHandleOpenNav = () => {
+    setOpen(prev => !prev)
+  }
+
+  const onHandleScroll = () => {
+    const y = window.scrollY
+
+    if (y > 0) {
+      setScroll(true)
+    } else {
+      setScroll(false)
+    }
+  }
+
+  useEffect(() => {
+    window.addEventListener('scroll', onHandleScroll)
+
+    return () => window.removeEventListener('scroll', onHandleScroll)
+  }, [])
+
+  return (
+    <div className={`flex justify-between items-center px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48 fixed bg-white ${scroll && 'shadow-md'} duration-700 transition-shadow top-0 left-0 right-0 h-16 z-10`}>
+      {/* Nav Desktop */}
+      <div className='gap-4 hidden md:flex'>
+        {
+          links.map((link, index) => (
+            <Link key={link.title} href={link.path} className={`p-1 rounded-md ${pathname === link.path && 'bg-black text-white'}`}>{link.title}</Link>
+          ))
+        }
+      </div>
+      {/* Logo */}
+      <div className={`${open ? 'bg-white' : 'bg-black'} p-1 font-bold rounded select-none cursor-pointer relative z-20`}>
+        <span className={`${open ? 'text-black' : 'text-white'}`}>Nazirov</span>
+        <span className={`${open ? 'text-white bg-black' : 'text-black bg-white'} px-2 ml-1 rounded`}>Dev.</span>
+      </div>
+      {/* Nav Mobile */}
+      <div className='block md:hidden relative z-20'>
+        <button className='w-10 h-8 flex flex-col justify-between' onClick={onHandleOpenNav}>
+          <motion.span
+            className={`w-full h-1 bg-yellow-500 block rounded-full`}
+            variants={variantTopNav}
+            animate={open ? 'open' : 'close'}></motion.span>
+          <motion.span
+            className={`w-full h-1 bg-yellow-500 block rounded-full`}
+            variants={variantCenterNav}
+            animate={open ? 'open' : 'close'}></motion.span>
+          <motion.span
+            className={`w-full h-1 bg-yellow-500 block rounded-full`}
+            variants={variantBottomNav}
+            animate={open ? 'open' : 'close'}></motion.span>
+        </button>
+      </div>
+      {
+        open && (
+          // <motion.div className='w-screen h-screen bg-black absolute left-0 top-0' variants={variantNav} initial="close" animate="open" >
+          //   <ul className='h-full w-full flex flex-col justify-center items-center gap-y-8'>
+          //     {
+          //       links.map(link => (
+          //         <motion.div>
+          //           <Link className='text-white text-4xl' href={link.path} key={link.title} onClick={onHandleOpenNav}>{link.title}</Link>
+          //         </motion.div>
+          //       ))
+          //     }
+          //   </ul>
+          // </motion.div>
+          <motion.div className='w-screen h-screen bg-black absolute left-0 top-0 flex flex-col justify-center items-center gap-y-8' variants={variantNav} initial="close" animate="open" >
+            {
+              links.map(link => (
+                <motion.div variants={variantNavItem}>
+                  <Link className='text-white text-4xl' href={link.path} key={link.title} onClick={onHandleOpenNav}>{link.title}</Link>
+                </motion.div>
+              ))
+            }
+          </motion.div>
+        )
+      }
+    </div>
+  )
+}
